2ec0ad37fbabf4da1baf8e3acc593ba51109fff5
Bruce <weichou1229@gmail.com>
2019-03-27 17:56:55 +0800

    Change import package name logging to logger
    Signed-off-by: Bruce <weichou1229@gmail.com>

--- internal/driver/driver_test.go
+++ internal/driver/driver_test.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/edgexfoundry/go-mod-core-contracts/clients/logging"
+	"github.com/edgexfoundry/go-mod-core-contracts/clients/logger"
 )
 
 func init() {
