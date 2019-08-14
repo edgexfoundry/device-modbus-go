aefaaed244e037047444cf8e3383dea6519a412f
Bruce <weichou1229@gmail.com>
2019-08-14 14:07:28 +0800

    Extract common function to check undefined value
    Signed-off-by: Bruce <weichou1229@gmail.com>

--- internal/driver/caster.go
+++ internal/driver/caster.go
@@ -6,16 +6,16 @@ import (
 )
 
 func toString(i interface{}) (string, error) {
-	if i == nil {
-		return "", fmt.Errorf("value is undefined")
+	if err := validateAttributeValue(i); err != nil {
+		return "", err
 	}
 	return fmt.Sprintf("%v", i), nil
 }
 
 func toUint16(i interface{}) (uint16, error) {
 	var val uint16 = 0
-	if i == nil {
-		return val, fmt.Errorf("value is undefined")
+	if err := validateAttributeValue(i); err != nil {
+		return val, err
 	}
 	stringVal := fmt.Sprintf("%v", i)
 	uint64Val, err := strconv.ParseUint(stringVal, 10, 16)
@@ -28,8 +28,8 @@ func toUint16(i interface{}) (uint16, error) {
 
 func toBool(i interface{}) (bool, error) {
 	var val = false
-	if i == nil {
-		return val, fmt.Errorf("value is undefined")
+	if err := validateAttributeValue(i); err != nil {
+		return val, err
 	}
 	stringVal := fmt.Sprintf("%v", i)
 	val, err := strconv.ParseBool(stringVal)
@@ -37,4 +37,12 @@ func toBool(i interface{}) (bool, error) {
 		return val, fmt.Errorf("parse to bool failed, error: %v", err)
 	}
 	return val, nil
+}
+
+func validateAttributeValue(i interface{}) error {
+	if i == nil {
+		return fmt.Errorf("value is undefined")
+	} else {
+		return nil
+	}
 }
