9bc48e746bb510c782b9593f55beae1b8e3bd068
Mengyi Wang <mengyi.wang@canonical.com>
2023-03-22 18:21:38 +0100

    fix(snap): Refactor to avoid conflicts with readonly config provider directory (#437)
    Signed-off-by: Mengyi Wang <mengyi.wang@canonical.com>

--- snap/local/bin/source-env-file.sh
+++ snap/local/bin/source-env-file.sh
@@ -1,17 +1,5 @@
 #!/bin/bash -e
 
-EDGEX_STARTUP_DURATION=$(snapctl get startup-duration)
-
-if [ -n "$EDGEX_STARTUP_DURATION" ]; then
-  export EDGEX_STARTUP_DURATION
-fi
-
-EDGEX_STARTUP_INTERVAL=$(snapctl get startup-interval)
-
-if [ -n "$EDGEX_STARTUP_INTERVAL" ]; then
-  export EDGEX_STARTUP_INTERVAL
-fi
-
 # convert cmdline to string array
 ARGV=($@)
 
@@ -20,14 +8,14 @@ BINPATH="${ARGV[0]}"
 
 # binary name == service name/key
 SERVICE=$(basename "$BINPATH")
-ENV_FILE="$SNAP_DATA/config/$SERVICE/res/$SERVICE.env"
+ENV_FILE="$SNAP_DATA/config/$SERVICE/overrides.env"
 TAG="edgex-$SERVICE."$(basename "$0")
 
 if [ -f "$ENV_FILE" ]; then
     logger --tag=$TAG "sourcing $ENV_FILE"
     set -o allexport
     source "$ENV_FILE" set
-    set +o allexport 
+    set +o allexport
 fi
 
 exec "$@"
--- snap/local/helper-go/go.mod
+++ snap/local/helper-go/go.mod
@@ -1,5 +1,5 @@
 module github.com/edgexfoundry/device-modbus-go/snap/local/helper-go
 
-require github.com/canonical/edgex-snap-hooks/v3 v3.0.0-20230112170125-c0580fb68dab
+require github.com/canonical/edgex-snap-hooks/v3 v3.0.0-20230315153603-544c6823a1b0
 
 go 1.20
