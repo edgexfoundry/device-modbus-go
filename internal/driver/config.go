487d9a8596a33bf8cb167ee9758ffd227ee0e3c5
lwq <liuwenqian@jxcc.com>
2025-01-06 13:29:05 +0800

    feat: support Modbus ASCII
    Signed-off-by: lwq <liuwenqian@jxcc.com>

--- internal/driver/config.go
+++ internal/driver/config.go
@@ -40,22 +40,33 @@ func (info *ConnectionInfo) String() string {
 
 func createConnectionInfo(protocols map[string]models.ProtocolProperties) (info *ConnectionInfo, err error) {
 	protocolRTU, rtuExist := protocols[ProtocolRTU]
+	protocolASCII, asciiExist := protocols[ProtocolASCII]
 	protocolTCP, tcpExist := protocols[ProtocolTCP]
 
-	if rtuExist && tcpExist {
-		return info, fmt.Errorf("unsupported multiple protocols, please choose %s or %s, not both", ProtocolRTU, ProtocolTCP)
-	} else if !rtuExist && !tcpExist {
+	if rtuExist && tcpExist || rtuExist && asciiExist || tcpExist && asciiExist || rtuExist && tcpExist && asciiExist {
+		driver.Logger.Errorf("unsupported multiple protocols,protocols: %+v", protocols)
+		return info, fmt.Errorf("unsupported multiple protocols, protocols:%+v", protocols)
+	} else if !rtuExist && !tcpExist && !asciiExist {
+		driver.Logger.Errorf("unable to create connection info,protocols: %+v", protocols)
 		return info, fmt.Errorf("unable to create connection info, protocol config '%s' or %s not exist", ProtocolRTU, ProtocolTCP)
 	}
 
 	if rtuExist {
 		info, err = createRTUConnectionInfo(protocolRTU)
 		if err != nil {
+			driver.Logger.Errorf("protocolRTU %+v create RTUConnectionInfo failed", protocolRTU)
+			return nil, err
+		}
+	} else if asciiExist {
+		info, err = createASCIIConnectionInfo(protocolASCII)
+		if err != nil {
+			driver.Logger.Errorf("protocolASCII %+v create ASCIIConnectionInfo failed", protocolASCII)
 			return nil, err
 		}
 	} else if tcpExist {
 		info, err = createTcpConnectionInfo(protocolTCP)
 		if err != nil {
+			driver.Logger.Errorf("protocolTCP %+v create TCPConnectionInfo failed", protocolTCP)
 			return nil, err
 		}
 	}
@@ -77,9 +88,9 @@ func parseIntValue(properties map[string]any, key string) (int, error) {
 	return res, nil
 }
 
-func createRTUConnectionInfo(rtuProtocol map[string]any) (info *ConnectionInfo, err error) {
+func createSerialConnectionInfo(protocol map[string]any) (info *ConnectionInfo, err error) {
 	errorMessage := "unable to create RTU connection info, protocol config '%s' not exist"
-	value, ok := rtuProtocol[Address]
+	value, ok := protocol[Address]
 	if !ok {
 		return nil, fmt.Errorf(errorMessage, Address)
 	}
@@ -88,7 +99,7 @@ func createRTUConnectionInfo(rtuProtocol map[string]any) (info *ConnectionInfo, 
 		return nil, fmt.Errorf("cannot transform '%s' value %v to string", Address, value)
 	}
 
-	us, ok := rtuProtocol[UnitID]
+	us, ok := protocol[UnitID]
 	if !ok {
 		return nil, fmt.Errorf(errorMessage, UnitID)
 	}
@@ -97,22 +108,22 @@ func createRTUConnectionInfo(rtuProtocol map[string]any) (info *ConnectionInfo, 
 		return nil, fmt.Errorf("uintID value out of range(0–255). Error: %v", err)
 	}
 
-	baudRate, err := parseIntValue(rtuProtocol, BaudRate)
+	baudRate, err := parseIntValue(protocol, BaudRate)
 	if err != nil {
 		return nil, err
 	}
 
-	dataBits, err := parseIntValue(rtuProtocol, DataBits)
+	dataBits, err := parseIntValue(protocol, DataBits)
 	if err != nil {
 		return nil, err
 	}
 
-	stopBits, err := parseIntValue(rtuProtocol, StopBits)
+	stopBits, err := parseIntValue(protocol, StopBits)
 	if err != nil {
 		return nil, err
 	}
 
-	value, ok = rtuProtocol[Parity]
+	value, ok = protocol[Parity]
 	if !ok {
 		return nil, fmt.Errorf(errorMessage, Parity)
 	}
@@ -124,18 +135,17 @@ func createRTUConnectionInfo(rtuProtocol map[string]any) (info *ConnectionInfo, 
 		return nil, fmt.Errorf("invalid parity value, it should be N(None) or O(Odd) or E(Even)")
 	}
 
-	timeout, err := parseIntValue(rtuProtocol, Timeout)
+	timeout, err := parseIntValue(protocol, Timeout)
 	if err != nil {
 		return nil, err
 	}
 
-	idleTimeout, err := parseIntValue(rtuProtocol, IdleTimeout)
+	idleTimeout, err := parseIntValue(protocol, IdleTimeout)
 	if err != nil {
 		return nil, err
 	}
 
 	return &ConnectionInfo{
-		Protocol:    ProtocolRTU,
 		Address:     address,
 		BaudRate:    baudRate,
 		DataBits:    dataBits,
@@ -144,6 +154,42 @@ func createRTUConnectionInfo(rtuProtocol map[string]any) (info *ConnectionInfo, 
 		UnitID:      byte(unitID),
 		Timeout:     timeout,
 		IdleTimeout: idleTimeout,
+	}, nil
+}
+
+func createRTUConnectionInfo(rtuProtocol map[string]any) (info *ConnectionInfo, err error) {
+	rtuInfo, err := createSerialConnectionInfo(rtuProtocol)
+	if err != nil {
+		return nil, err
+	}
+	return &ConnectionInfo{
+		Protocol:    ProtocolRTU,
+		Address:     rtuInfo.Address,
+		BaudRate:    rtuInfo.BaudRate,
+		DataBits:    rtuInfo.DataBits,
+		StopBits:    rtuInfo.StopBits,
+		Parity:      rtuInfo.Parity,
+		UnitID:      rtuInfo.UnitID,
+		Timeout:     rtuInfo.Timeout,
+		IdleTimeout: rtuInfo.IdleTimeout,
+	}, nil
+}
+
+func createASCIIConnectionInfo(asciiProtocol map[string]any) (info *ConnectionInfo, err error) {
+	asciiInfo, err := createSerialConnectionInfo(asciiProtocol)
+	if err != nil {
+		return nil, err
+	}
+	return &ConnectionInfo{
+		Protocol:    ProtocolASCII,
+		Address:     asciiInfo.Address,
+		BaudRate:    asciiInfo.BaudRate,
+		DataBits:    asciiInfo.DataBits,
+		StopBits:    asciiInfo.StopBits,
+		Parity:      asciiInfo.Parity,
+		UnitID:      asciiInfo.UnitID,
+		Timeout:     asciiInfo.Timeout,
+		IdleTimeout: asciiInfo.IdleTimeout,
 	}, nil
 }
 
